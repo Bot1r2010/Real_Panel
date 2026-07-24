d7efb3e4d0d768772f3fa48eb9d3b9204e216448
neymar-xx <echidnamygirlwife@gmail.com>
2026-07-24 17:30:42 +0000

    features/sidebar

--- src/components/saiidbar/Sidebar.jsx
+++ src/components/saiidbar/Sidebar.jsx
@@ -5,21 +5,26 @@ import Link from 'next/link';
 export default function Sidebar() {
   return (
     <aside className="w-64 bg-white border-r border-gray-200 p-4 min-h-screen text-gray-800 shadow-xs">
+      <div className=' mb-4'>
+        <svg width="58" height="58" viewBox="0 0 46 46" className="shrink-0">
+            <circle cx="23" cy="23" r="19" fill="none" stroke="#d6293a" strokeWidth="2.5" opacity="0.35" />
+            <circle cx="23" cy="23" r="12" fill="none" stroke="#d6293a" strokeWidth="3" opacity="0.6" />
+            <circle cx="23" cy="23" r="6" fill="#d6293a" />
+            <circle cx="23" cy="23" r="2" fill="#ffffff" />
+          </svg>
+      </div>
       <nav className="space-y-1">
-        <Link href="/dashboard" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-blue-600 transition">
+        <Link href="/dashboard" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-950 transition">
           Dashboard
         </Link>
-        <Link href="/products" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-blue-600 transition">
+        <Link href="/products" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-950 transition">
           Products
         </Link>
-        <Link href="/users" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-blue-600 transition">
+        <Link href="/users" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-950 transition">
           Users
         </Link>
-        <Link href="/profile" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-blue-600 transition">
+        <Link href="/profile" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-950 transition">
           Profile
-        </Link>
-        <Link href="/doc" className="block py-2.5 px-3 rounded-md font-medium text-gray-700 hover:bg-gray-100 hover:text-blue-600 transition">
-          Documentation
         </Link>
       </nav>
     </aside>
