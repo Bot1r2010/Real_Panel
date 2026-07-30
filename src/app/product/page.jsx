454a085b62d98e17e36567813edef5b7e399acc7
Bot1r2010 <batirusman2010@gmail.com>
2026-07-30 17:55:59 +0500

    features/categories

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,9 +1,3 @@
-import React from 'react'
-
-const products = () => {
-  return (
-    <div>products</div>
-  )
+export default function Page() {
+  return <div></div>;
 }
-
-export default products
